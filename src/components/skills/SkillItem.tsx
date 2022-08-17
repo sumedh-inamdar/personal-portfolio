360773ca02a1cfc39c5d5e8d25b0e2ae4fc2b89b
Sumedh Inamdar <sinamdar@gmail.com>
2022-08-17 08:34:28 -0700

    Added height / width attributes to SVG icons, removed min width from img in SkillItem

--- src/components/skills/SkillItem.tsx
+++ src/components/skills/SkillItem.tsx
@@ -7,12 +7,7 @@ export default function SkillItem({ name, src }: SkillProps) {
   return (
     <div className="h-48 flex flex-col items-center transition-all hover:bg-blue hover:text-shade0 font-light hover:font-bold animateFadeinUp opacity-0 border border-shade4 rounded-md p-4 shadow-lg">
       <div className="flex-1 flex flex-col justify-center">
-        <img
-          src={src}
-          className="max-h-[100px] min-h-[70px]"
-          role="img"
-          alt={name}
-        />
+        <img src={src} className="max-h-[100px]" role="img" alt={name} />
       </div>
       <div className="text-lg">{name}</div>
     </div>
